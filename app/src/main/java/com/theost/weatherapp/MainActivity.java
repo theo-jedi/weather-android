7c70209cab30e2c463d4444f19afbfdb7b7e4cc4
fedor-jedi <fedor.contact@yandex.ru>
2021-03-21 10:07:23 +0300

    Add ListView

--- app/src/main/java/com/theost/weatherapp/MainActivity.java
+++ app/src/main/java/com/theost/weatherapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.theost.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.city_listview);
+
+        // определяем строковый массив
+        final String[] catNames = new String[] {
+                "Алмазный", "Западный", "Курортный", "Лесной", "Научный",
+                "Полярный", "Портовый", "Приморский", "Садовый", "Северный",
+                "Степной", "Таежный", "Южный"
+        };
+
+        // используем адаптер данных
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, catNames);
+
+        listView.setAdapter(adapter);
     }
+
 }
