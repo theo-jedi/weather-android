6822bd1db031b2252f8a07d531afb3e969710571
Студент <koshetok.com@mail.ru>
2021-03-21 16:32:06 +0300

    UI2

--- app/src/main/java/com/theost/weatherapp/GraphFragment.java
+++ app/src/main/java/com/theost/weatherapp/GraphFragment.java
@@ -10,27 +10,37 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.button.MaterialButtonToggleGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GraphFragment extends Fragment {
 
     private static final String ARG_PARAM1 = "MONTH";
     private static final String ARG_PARAM2 = "DAY";
     private static final String ARG_PARAM3 = "WEEK";
+    private static final String ARG_PARAM4 = "TEMPS";
 
     private String mParam1;
     private String mParam2;
     private String mParam3;
+    private float[] mParam4;
 
     public GraphFragment() {}
 
-    public static GraphFragment newInstance(String param1, String param2, String param3) {
+    public static GraphFragment newInstance(String param1, String param2, String param3, float[] param4) {
         GraphFragment fragment = new GraphFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         args.putString(ARG_PARAM3, param3);
+        args.putFloatArray(ARG_PARAM4, param4);
         fragment.setArguments(args);
         return fragment;
     }
@@ -42,6 +52,7 @@ public class GraphFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
             mParam3 = getArguments().getString(ARG_PARAM3);
+            mParam4 = getArguments().getFloatArray(ARG_PARAM4);
         }
     }
 
@@ -50,6 +61,7 @@ public class GraphFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View layout = inflater.inflate(R.layout.fragment_graph, container, false);
+        LineChart chart = (LineChart) layout.findViewById(R.id.chart);
 //        TextView tv = layout.findViewById(R.id.textView);
 //        MaterialButtonToggleGroup toggleButtons = layout.findViewById(R.id.toggleButton);
 //
@@ -60,6 +72,37 @@ public class GraphFragment extends Fragment {
 //                         break;
 //                 }
 //        });
+        DATA[] dataObjects = new DATA[365];
+        for (int i = 0; i < 365; i++) {
+            dataObjects[i] = new DATA(i, mParam4[i]);
+        }
+        List<Entry> entries = new ArrayList<Entry>();
+        for (DATA data : dataObjects) {
+            // turn your data into Entry objects
+            entries.add(new Entry(data.getValueX(), data.getValueY()));
+        }
+        LineDataSet dataSet = new LineDataSet(entries, "Label"); // add entries to dataset
+        dataSet.setColor(R.color.design_default_color_on_primary);
+        dataSet.setValueTextColor(R.color.design_default_color_primary_dark); // styling, ...
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
         return layout;
+    }
+    class DATA {
+        float X;
+        float Y;
+
+        public DATA(float x, float y) {
+            X = x;
+            Y = y;
+        }
+
+        float getValueX() {
+            return X;
+        }
+        float getValueY() {
+            return Y;
+        }
     }
 }
